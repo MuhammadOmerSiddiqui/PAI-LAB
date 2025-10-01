03f6f416f82dc881a9f29419e2a863f9db203faa
Muhammad Omer Siddiqui <siddiquiomar738@gmail.com>
2025-10-02 00:12:32 +0500

    Create Task1.py

---
+++ LAB 1/Task1.py
@@ -0,0 +1,11 @@
+number = int(input("Enter a number:"))
+float_number = float(number)
+string_number = str(number)
+complex_number = complex(number)
+print(f"Float transformation is {float_number}")
+print(f"String transformation is {string_number}")
+print(f"Complex transformation is {complex_number}")
+if(number//2==(number-1)/2):
+    print("The number is divisible by 2")
+else:
+    print("The number is not divisible by 2")
